051a14f9970858a557cc5d047f3935693321e537
niuding <nding@qq.com>
2018-07-02 15:00:39 +0800

    more As5045 and use us delay

--- AS5045.cpp
+++ AS5045.cpp
@@ -1,6 +1,7 @@
 #include "Arduino.h"
 #include "AS5045.h"
 
+
 AS5045::AS5045(uint16_t DataPin, uint16_t ClockPin, uint16_t ChipSelectPin)
            : _data(DataPin), _clock(ClockPin), _cs(ChipSelectPin)
 {
@@ -10,21 +11,21 @@ AS5045::AS5045(uint16_t DataPin, uint16_t ClockPin, uint16_t ChipSelectPin)
 }
 
 
-uint32_t AS5045::encoder_degrees(void)
+uint32_t AS5045::encoder_degrees(uint16_t Number)
 {
-  return ((encoder_value() * 360)/4096);
+  return (((read_chip(Number) >> 6)* 360)/4096);
 }
 
-uint32_t AS5045::encoder_value(void)
+uint32_t AS5045::encoder_value(uint16_t Number)
 {
-  return (read_chip() >> 6);
+  return (read_chip(Number) >> 6);
 }
 
-uint32_t AS5045::encoder_error(void)
+uint32_t AS5045::encoder_error(uint16_t Number)
 {
   uint16_t error_code;
   uint32_t raw_value;
-  raw_value = read_chip();
+  raw_value = read_chip(Number);
   error_code = raw_value & 0b000000000000111111;
   err_value.DECn = error_code & 2;
   err_value.INCn = error_code & 4;
@@ -34,29 +35,36 @@ uint32_t AS5045::encoder_error(void)
   return error_code;
 }
 
-uint32_t AS5045::read_chip(void)
+uint32_t AS5045::read_chip(uint16_t Number)
 {
   uint32_t raw_value = 0;
   uint16_t inputstream = 0;
-  uint16_t c;
+  uint16_t c,n;
   digitalWrite(_cs, HIGH);
   digitalWrite(_clock, HIGH);
-  delay(100);
+  delayMicroseconds(10);
   digitalWrite(_cs, LOW);
-  delay(10);
+  delayMicroseconds(5);
   digitalWrite(_clock, LOW);
-  delay(10);
-  for (c = 0; c < 18; c++)
+  delayMicroseconds(5);
+  for(n = 0; n < Number; n++)
   {
-    digitalWrite(_clock, HIGH);
-    delay(10);
-    inputstream = digitalRead(_data);
-    raw_value = ((raw_value << 1) + inputstream);
-    digitalWrite(_clock, LOW);
-    delay(10);
+  	  raw_value = 0;
+	  for (c = 0; c < 18; c++)
+	  {
+	    digitalWrite(_clock, HIGH);
+	    delayMicroseconds(5);
+	    inputstream = digitalRead(_data);
+	    raw_value = ((raw_value << 1) + inputstream);
+	    digitalWrite(_clock, LOW);
+	    delayMicroseconds(5);
+	  }
+	   digitalWrite(_clock, HIGH);
+	   delayMicroseconds(5);
+	   digitalWrite(_clock, LOW);
+	   delayMicroseconds(5);
   }
  return raw_value;
 }
-  
 
 
--- AS5045.h
+++ AS5045.h
@@ -7,21 +7,21 @@ class AS5045
 {
   public:
     AS5045(uint16_t DataPin, uint16_t ClockPin, uint16_t ChipSelectPin);
-    uint32_t encoder_degrees(void);
-    uint32_t encoder_value(void);
-    uint32_t encoder_error(void);
+    uint32_t encoder_degrees(uint16_t Number);
+    uint32_t encoder_value(uint16_t Number);
+    uint32_t encoder_error(uint16_t Number);
     struct err_struct{
   	bool DECn;
 	bool INCn;
 	bool OCF;
 	bool COF;
 	bool LIN; } err_value;
-  private:
-    uint32_t read_chip(void);
+  public:
+    uint32_t read_chip(uint16_t Number);
     const uint16_t _clock;        // clock pin: output from arduino to as5045
     const uint16_t _cs;           // chip select: output
     const uint16_t _data;         // data pin: input
-    
+    //nt32_t encode_number[6]={0,0,0,0,0,0};
 };
 
 #endif
